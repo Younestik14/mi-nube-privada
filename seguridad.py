b2270f0fe6e10f3d21631a9e0bab547499dac6fc
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:48:51 +0200

    Delete seguridad.py

--- seguridad.py
+++
@@ -1,7 +0,0 @@
-# seguridad.py
-import streamlit as st
-
-def verificar_sesion():
-    if not st.session_state.get('autorizado', False):
-        st.error("Debes iniciar sesión primero.")
-        st.stop()
