e353a70819fd88872d7e7325fac3fb0e99cf5829
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:58:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-from estilos import aplicar_marca_agua
 
-# Llamamos a la función justo después de cualquier configuración inicial
-aplicar_marca_agua()
-
-# ... resto de tu código ...
 
 # Título Principal
 st.title("⚡ CoreElec: Plataforma de Ingeniería Eléctrica")
