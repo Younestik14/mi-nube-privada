c728135d51e819348d15f82581445e3257854782
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:53:56 +0200

    Update style.py

--- utils/style.py
+++ utils/style.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
+def aplicar_marca_agua():
+    marca_agua = """
+    <style>
+    .watermark {
+        position: fixed;
+        bottom: 10px;
+        right: 20px;
+        color: rgba(128, 128, 128, 0.3); /* Color gris con transparencia */
+        font-size: 20px;
+        font-weight: bold;
+        z-index: 1000;
+        pointer-events: none; /* Esto evita que tape los botones o clicks */
+    }
+    </style>
+    <div class="watermark">Younesse Tikent Tifaoui</div>
+    """
+    st.markdown(marca_agua, unsafe_allow_html=True)
 def aplicar_estilo_global():
     """Inyecta el CSS avanzado para el Modo Oscuro Premium en toda la web."""
     st.markdown("""
