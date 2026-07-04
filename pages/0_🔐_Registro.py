bf0606b0494fc8e6fc000287854d000402bdfa5f
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:33:14 +0200

    Update 0_🔐_Registro.py

--- pages/0_🔐_Registro.py
+++ pages/0_🔐_Registro.py
@@ -1,11 +1,5 @@
-import sys
-import os
 import streamlit as st
 # (No necesitas importar init_db ni registrar_usuario de auth.py si usas la lista blanca)
 
 st.title("🔐 Registro de Acceso")
 st.write("Si necesitas acceso, contacta con el administrador.")
-# Esto le dice a Python: "busca en la carpeta superior donde están los demás archivos"
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from auth import init_db, registrar_usuario
