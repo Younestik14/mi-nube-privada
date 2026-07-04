5fdcef645bbb73fd974039d03472cbd0e94ba587
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:49:38 +0200

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import streamlit as st
-
-# Código simple para ver quién ha pedido entrar
-conn = sqlite3.connect('usuarios.db')
-data = conn.execute("SELECT * FROM usuarios").fetchall()
-st.table(data)
