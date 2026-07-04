3b06d077e4d42f892a20f47ab6cbbf511f5cc182
Younesse Tikent Tifaoui <159943037+Younestik14@users.noreply.github.com>
2026-07-04 12:31:45 +0200

    Update 0_🔐_Registro.py

--- pages/0_🔐_Registro.py
+++ pages/0_🔐_Registro.py
@@ -1,14 +1,7 @@
-import streamlit as st
+import sys
+import os
+
+# Esto le dice a Python: "busca en la carpeta superior donde están los demás archivos"
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from auth import init_db, registrar_usuario
-
-init_db()
-
-st.title("🔐 Registro de Acceso")
-with st.form("registro"):
-    nombre = st.text_input("Nombre completo:")
-    num = st.text_input("Número regional:")
-    if st.form_submit_button("Enviar"):
-        if registrar_usuario(nombre, num):
-            st.success("¡Enviado! Espera a que el administrador te apruebe.")
-        else:
-            st.error("Error: ese número ya existe.")
